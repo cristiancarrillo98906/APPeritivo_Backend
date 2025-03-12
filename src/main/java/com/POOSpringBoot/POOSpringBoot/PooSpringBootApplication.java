9a497f468d27d341efe34df3005f5c2f61776eba
cristian carrillo <cristiancarrillo98906@gmail.com>
2025-03-12 20:08:13 +0100

    restau, usu, event, favo, receta, reserv, reseña

--- src/main/java/com/POOSpringBoot/POOSpringBoot/PooSpringBootApplication.java
+++ src/main/java/com/POOSpringBoot/POOSpringBoot/PooSpringBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PooSpringBootApplication {
-
+	//APPeritivo Main
 	public static void main(String[] args) {
 		SpringApplication.run(PooSpringBootApplication.class, args);
 	}
